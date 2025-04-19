dd2970ea6927c0a14b45149b51a3927439d459d4
fe_qianzw <fe_qianzw@foxmail.com>
2025-04-19 11:00:27 +0800

    element-plus 自定义命名空间问题复现

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-config-provider namespace="ep">
+    <div>
+      <el-button @click="open1">弹出message</el-button>
+      <el-button @click="open2">弹出message-box</el-button>
+    </div>
+  </el-config-provider>
+</template>
+<script setup>
+function open1() {
+  ElMessage.info('This is a message')
+}
+function open2() {
+  ElMessageBox.confirm('proxy will permanently delete the file. Continue?', 'Warning', {
+    confirmButtonText: 'OK',
+    cancelButtonText: 'Cancel',
+    type: 'warning'
+  })
+    .then(() => {
+      ElMessage({
+        type: 'success',
+        message: 'Delete completed'
+      })
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: 'Delete canceled'
+      })
+    })
+}
+</script>
+<style scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { resolve } from 'node:path'
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+export default ({ mode }) => {
+  return defineConfig({
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/element/index.scss" as *;`
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        imports: ['vue'],
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'sass'
+          })
+        ]
+      })
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: 3000
+    }
+  })
+}
